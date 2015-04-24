e3b6aa4266f3971467adc700315e594f1f2ce421
qianyu668899 <jinggeqianyu1991@163.com>
2015-04-24 10:14:54 -0400

    Add example to connect to mongoDB using casbah

--- HelloTest/src/main/scala/com/yu/start/package.scala
+++ HelloTest/src/main/scala/com/yu/start/package.scala
@@ -3,8 +3,19 @@ package com.yu
 /**
  * Created by Yu on 15-04-24.
  */
+
+import com.mongodb.casbah.Imports._
+
 package object start {
   def main(args: Array[String]) {
     println("Hello, My world!")
+    val mongoClient = MongoClient("localhost", 27017)
+    val db = mongoClient("test")
+    db.collectionNames
+    val coll = db("test")
+    val a = MongoDBObject("hello" -> "world")
+    val b = MongoDBObject("language" -> "scala")
+    coll.insert( a )
+    coll.insert( b )
   }
 }
