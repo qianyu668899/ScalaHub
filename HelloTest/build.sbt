0a9ccee96ba13a4a42dc6e6d46eb99fb1b31068e
qianyu668899 <jinggeqianyu1991@163.com>
2015-04-24 11:43:18 -0400

    Implement simple CRUD using casbah

--- HelloTest/build.sbt
+++ HelloTest/build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.6"
 
 libraryDependencies += "org.mongodb" %% "casbah"% "2.7.3"
+
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
--- HelloTest/src/main/scala/com/yu/start/package.scala
+++ HelloTest/src/main/scala/com/yu/start/package.scala
@@ -8,14 +8,56 @@ import com.mongodb.casbah.Imports._
 
 package object start {
   def main(args: Array[String]) {
-    println("Hello, My world!")
     val mongoClient = MongoClient("localhost", 27017)
-    val db = mongoClient("test")
+    insert(mongoClient, "test")
+    println(getCount(mongoClient, "test"))
+    getCursor(mongoClient, "test")
+    update(mongoClient, "test")
+    remove(mongoClient, "test")
+    drop(mongoClient, "test")
+  }
+
+  def insert(mongoClient: MongoClient, dbName:String): Unit = {
+    val db = mongoClient(dbName)
     db.collectionNames
-    val coll = db("test")
+    val coll = db(dbName)
     val a = MongoDBObject("hello" -> "world")
     val b = MongoDBObject("language" -> "scala")
     coll.insert( a )
     coll.insert( b )
+  }
+
+  def getCount(mongoClient: MongoClient, dbName:String): Int = {
+    return mongoClient(dbName)(dbName).count()
+  }
+
+  def getCursor(mongoClient: MongoClient, dbName:String): MongoCursor = {
+    val allDocs =  mongoClient(dbName)(dbName).find()
+    println( allDocs )
+    for(doc <- allDocs) println( doc )
+    return allDocs
+  }
+
+  def update(mongoClient: MongoClient, dbName:String): Unit = {
+    val coll = mongoClient(dbName)(dbName)
+    val query = MongoDBObject("hello" -> "world")
+    val update = MongoDBObject("platform" -> "iOS")
+    val result = coll.update( query, update )
+
+    println("Number updated: " + result.getN)
+    for (c <- coll.find) println(c)
+  }
+
+  def remove(mongoClient: MongoClient, dbName:String): Unit = {
+    val coll = mongoClient(dbName)(dbName)
+    val query = MongoDBObject("language" -> "scala")
+    val result = coll.remove( query )
+
+    println("Number removed: " + result.getN)
+    for (c <- coll.find) println(c)
+  }
+
+  def drop(mongoClient: MongoClient, dbName:String): Unit = {
+    mongoClient(dbName)(dbName).drop()
   }
 }
