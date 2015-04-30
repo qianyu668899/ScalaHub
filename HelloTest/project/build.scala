f4497bb64be709bc5dc972c3133fc67921963e81
qianyu668899 <jinggeqianyu1991@163.com>
2015-04-30 17:16:30 -0400

    add build.scala which will replace build.sbt in the future

--- HelloTest/project/build.scala
+++ HelloTest/project/build.scala
@@ -1,8 +1,10 @@
 import sbtrelease.ReleasePlugin._
+import sbt._
+import Keys._
 
 object MyBuild extends Build {
-  lazy val myProject = Project(
-    id = "myproject",
+  lazy val HelloTest = Project(
+    id = "HelloTest",
     base = file("."),
     settings = Defaults.defaultSettings ++ releaseSettings ++ Seq( /* custom settings here */ )
   )
