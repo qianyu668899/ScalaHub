7dab377882304326a3f8dab1bf4b5db295e78b3d
qianyu668899 <jinggeqianyu1991@163.com>
2015-05-07 11:40:38 -0400

    Update project info

--- HelloTest/project/build.scala
+++ HelloTest/project/build.scala
@@ -14,6 +14,14 @@ object MyBuild extends Build {
         mongoDb,
         log
       )
+        publishTo <<= (version) { version: String =>
+        val scalasbt = "http://scalasbt.artifactoryonline.com/scalasbt/"
+        val (name, url) = if (version.contains("-SNAPSHOT"))
+          ("sbt-plugin-snapshots-publish", scalasbt+"sbt-plugin-snapshots")
+        else
+          ("sbt-plugin-releases-publish", scalasbt+"sbt-plugin-releases")
+        Some(Resolver.url(name, new URL(url))(Resolver.ivyStylePatterns))
+      }
     )
   )
 
